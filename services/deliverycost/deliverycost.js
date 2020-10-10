df8d65a0630f81dee86a60d3d300761a1cbeef8f
Supanut Apikulvanich <supanut.overload@gmail.com>
2020-10-10 23:34:32 +0700

    :sparkles: Delivery Cost Logic & Test

---
+++ services/deliverycost/deliverycost.js
@@ -0,0 +1,23 @@
+const { createGraph } = require('../../lib/graph')
+
+const calculateDeliveryCost = (graphtext, route) => {
+  const graph = createGraph(graphtext)
+  let [currentNode, ...nextNodes] = route.split('-')
+
+  let sumWeight = 0
+
+  for (let i = 0; i < nextNodes.length; i++) {
+    const nextNode = nextNodes[i]
+    const weight = graph.getEdgeWeight(currentNode, nextNode, 0)
+    // TODO: Check Perfomance
+    const pathExist = graph.adjacent(currentNode).includes(nextNode)
+    if (!pathExist) {
+      return 'No Such Route'
+    }
+    sumWeight += weight
+    currentNode = nextNode
+  }
+  return sumWeight
+}
+
+module.exports.calculateDeliveryCost = calculateDeliveryCost
---
+++ services/deliverycost/deliverycost.test.js
@@ -0,0 +1,17 @@
+const { calculateDeliveryCost } = require('./deliverycost')
+
+const GRAPH_TEXT = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
+
+const testDeliveryCost = (route, expectedCost) => {
+  it(`should calculate cost of ${route}`, () => {
+    expect(calculateDeliveryCost(GRAPH_TEXT, route)).toEqual(expectedCost)
+  })
+}
+
+describe(`DeliveryCost (${GRAPH_TEXT})`, () => {
+  testDeliveryCost('A-B-E', 4)
+  testDeliveryCost('A-D', 10)
+  testDeliveryCost('E-A-C-F', 8)
+  testDeliveryCost('A-D-F', 'No Such Route')
+  testDeliveryCost('A-F', 'No Such Route')
+})
