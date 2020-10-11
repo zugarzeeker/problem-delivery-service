8a26848b318598fd618b7e709d5ebe3648f5a49e
Supanut Apikulvanich <supanut.overload@gmail.com>
2020-10-12 00:53:00 +0700

    Use uuid instead hard code for dummy node

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const DEFAULT_PORT = 6000
 const PORT = process.env.PORT || DEFAULT_PORT
 
-app.use(morgan('dev'))
+// app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
