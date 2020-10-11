625f81c27ac820367a9f36ed4f5ed0f6df562b6f
Supanut Apikulvanich <supanut.overload@gmail.com>
2020-10-12 00:06:30 +0700

    Add log morgan

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const { createServices } = require('./services')
 const app = express()
@@ -6,6 +7,7 @@ const app = express()
 const DEFAULT_PORT = 6000
 const PORT = process.env.PORT || DEFAULT_PORT
 
+app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
