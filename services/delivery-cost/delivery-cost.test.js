de6f5dad5932390e11333492833c433eed62954c
Supanut Apikulvanich <supanut.overload@gmail.com>
2020-10-12 00:43:47 +0700

    Add more test cases

--- services/delivery-cost/delivery-cost.test.js
+++ services/delivery-cost/delivery-cost.test.js
@@ -2,18 +2,20 @@ const { calculateDeliveryCost } = require('./delivery-cost')
 
 const GRAPH_TEXT = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
 
-const testDeliveryCost = (route, expectedCost) => {
-  it(`should calculate cost of ${route}`, () => {
-    expect(calculateDeliveryCost(GRAPH_TEXT, route)).toEqual(expectedCost)
+const testDeliveryCost = (graphtext, route, expectedCost) => {
+  it(`should calculate delivery cost of ${route}`, () => {
+    expect(calculateDeliveryCost(graphtext, route)).toEqual(expectedCost)
   })
 }
 
 describe(`DeliveryCost (${GRAPH_TEXT})`, () => {
-  testDeliveryCost('A-B-E', 4)
-  testDeliveryCost('A-D', 10)
-  testDeliveryCost('E-A-C-F', 8)
-  testDeliveryCost('A-D-F', 'No Such Route')
-  testDeliveryCost('A-F', 'No Such Route')
-  testDeliveryCost('B-E-B-E-B', 12)
-  testDeliveryCost('A-A', 'No Such Route')
+  testDeliveryCost(GRAPH_TEXT, 'A-B-E', 4)
+  testDeliveryCost(GRAPH_TEXT, 'A-D', 10)
+  testDeliveryCost(GRAPH_TEXT, 'E-A-C-F', 8)
+  testDeliveryCost(GRAPH_TEXT, 'A-D-F', 'No Such Route')
+  testDeliveryCost(GRAPH_TEXT, 'A-F', 'No Such Route')
+  testDeliveryCost(GRAPH_TEXT, 'B-E-B-E-B', 12)
+  testDeliveryCost(GRAPH_TEXT, 'A-A', 'No Such Route')
+  testDeliveryCost('AA2', 'A-A', 2)
+  testDeliveryCost('AA0', 'A-A', 'No Such Route')
 })
